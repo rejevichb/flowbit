334eff88ece3f22adec3398a06c109155b91cd57
Danijj <danijj89@gmail.com>
2019-03-18 21:53:41 -0400

    added implementation of flowbit

--- src/flowbit/FlowBitImpl.scala
+++ src/flowbit/FlowBitImpl.scala
@@ -1,93 +1,91 @@
 package flowbit
-
 import scala.collection.mutable.HashMap
+import scala.collection.mutable.HashSet
 
-/**
-  * Flowbit API to build customized ETL pipelines using Kafka.
-  * How to use:
-  * - create topics where components will publish/subscribe to
-  * - add components
-  */
-trait FlowBit {
+class FlowBitImpl extends FlowBit {
+  var topics = new HashSet[String]
+  var units = new HashMap[String, Unit]
+
 
   /**
     * Adds a new Kafka topic into the system.
     *
     * @param topic the topic to be added.
     */
-  def addTopic(topic: String): Unit
+  override def addTopic(topic: String): Unit = {
+    topics.add(topic)
+  }
 
   /**
     * Adds a list of new topics into the system.
     *
     * @param topics list of topics.
     */
-  def addTopics(topics: List[String]): Unit
+  override def addTopics(topics: List[String]): Unit = ???
 
   /**
     * Creates a producer that sends the given data to the given list of topics.
     *
-    * @param data the data to be sent into the pipeline.
+    * @param data   the data to be sent into the pipeline.
     * @param topics the topics to which to send the data.
     * @tparam A the key data type
     * @tparam B the value data type
     */
-  def sendData[A,B](data: HashMap[A,B], topics: List[String]): Unit
+  override def sendData[A, B](data: HashMap[A, B], topics: List[String]): Unit = ???
 
   /**
     * Adds a new filter unit that filters the streams of data from a given {@code fromTopic}
     * to a given {@code toTopic}.
     *
-    * @param id the id of the unit.
+    * @param id        the id of the unit.
     * @param fromTopic the topic to which to subscribe to.
-    * @param toTopic the topic to which to publish to.
-    * @param pred the predicate of the filter.
+    * @param toTopic   the topic to which to publish to.
+    * @param pred      the predicate of the filter.
     * @tparam A the values beings processed.
     */
-  def addFilter[A](id: String, fromTopic: String, toTopic: String, pred: (A) => Boolean): Unit
+  override def addFilter[A](id: String, fromTopic: String, toTopic: String, pred: A => Boolean): Unit = ???
 
   /**
     * Adds a new map unit that executes a map {@code func} on a stream of data from a given
     * {@code fromTopic} to a given {@code toTopic}.
     *
-    * @param id the id of the unit.
+    * @param id        the id of the unit.
     * @param fromTopic the topic to which to subscribe to.
-    * @param toTopic the topic to which to publish to.
-    * @param func the map function.
+    * @param toTopic   the topic to which to publish to.
+    * @param func      the map function.
     * @tparam A the data type of the data to be processed.
     * @tparam B the data type of the processed data.
     */
-  def addMap[A,B](id: String, fromTopic: String, toTopic: String, func: (A) => B): Unit
+  override def addMap[A, B](id: String, fromTopic: String, toTopic: String, func: A => B): Unit = ???
 
   /**
     * Creates a consumer that returns the data from a given topic as a list.
     *
-    * @param id the id of the consumer.
+    * @param id    the id of the consumer.
     * @param topic the topic from which to get the values from.
     * @tparam A the data type of the values to be retrieved.
-    *
     * @return the values as a list
     */
-  def getConsumer[A](id: String, topic: String): List[A]
+  override def getConsumer[A](id: String, topic: String): List[A] = ???
 
   /**
     * Removes a unit from the pipeline.
     *
     * @param id the id of the unit.
     */
-  def removeUnit(id: String): Unit
+  override def removeUnit(id: String): Unit = ???
 
   /**
     * Returns the list of all units ids in this pipeline.
     *
     * @return the list of units ids.
     */
-  def getUnits(): List[String]
+  override def getUnits(): List[String] = ???
 
   /**
     * Returns the list of all topics in this pipeline.
     *
     * @return the list of topics.
     */
-  def getTopics(): List[String]
+  override def getTopics(): List[String] = ???
 }
