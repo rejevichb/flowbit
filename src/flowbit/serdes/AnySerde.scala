bc3fb905d36f50b80ed0cbdc85cbe323508dbd98
Danijj <danijj89@gmail.com>
2019-03-25 22:35:39 -0400

    Any type serde impl test

---
+++ src/flowbit/serdes/AnySerde.scala
@@ -0,0 +1,21 @@
+package flowbit.serdes
+
+import java.util
+
+import flowbit.serdes.serializer.AnySerializer
+import org.apache.kafka.common.serialization.{Deserializer, Serde, Serializer}
+
+class AnySerde extends Serde[Any] {
+  override def configure(map: util.Map[String, _], b: Boolean): Unit = {}
+
+  override def close(): Unit = {}
+
+  override def serializer(): Serializer[Any] = {
+    new AnySerializer
+  }
+
+  override def deserializer(): Deserializer[Any] = {
+    new AnyDeserializer
+  }
+}
+
